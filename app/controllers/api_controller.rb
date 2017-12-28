a79e7ef35ca38a9bad0ab8bb49adeb1df9b38c22
Stuart Olivera <stuart@stuartolivera.com>
2017-12-28 18:27:58 -0500

    Add JSON helpers
    Source: https://scotch.io/tutorials/build-a-restful-json-api-with-rails-5-part-one

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,9 @@
 class ApiController < ApplicationController
+  include Response
+  include ExceptionHandler
+
   def real_time_all
-    render json: CourseRealTime.all
+    json_response(CourseRealTime.all)
   end
 
   def import
