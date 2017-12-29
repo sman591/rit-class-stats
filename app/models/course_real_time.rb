39881965d326b25cb1bebb292d5d78ae8f37b31b
Stuart Olivera <stuart@stuartolivera.com>
2017-12-29 13:03:29 -0500

    Add basic model validations

--- app/models/course_real_time.rb
+++ app/models/course_real_time.rb
@@ -1,2 +1,6 @@
 class CourseRealTime < ApplicationRecord
+  validates_presence_of :college
+  validates_presence_of :snapshot_at
+  validates_presence_of :capacity_data
+  # validates_inclusion_of :college, in: %w( ... )
 end
--- test/models/course_real_time_test.rb
+++ test/models/course_real_time_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CourseRealTimeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of :college
+  should validate_presence_of :snapshot_at
+  should validate_presence_of :capacity_data
 end
