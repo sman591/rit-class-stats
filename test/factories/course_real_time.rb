53e62c88cb59b506ff3f68f53a4347c1f2c992bb
Stuart Olivera <stuart@stuartolivera.com>
2017-12-29 13:49:13 -0500

    Fix use of Fake::Color

--- test/factories/course_real_time.rb
+++ test/factories/course_real_time.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :course_real_time do
     snapshot_at Faker::Time.between(2.days.ago, Date.today)
     college Faker::Color.color_name
-    capacity_data { { foo: Faker::Color.name } }
+    capacity_data { { foo: Faker::Color.color_name } }
   end
 end
