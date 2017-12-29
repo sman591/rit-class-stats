2a35574e1f44deaf5224737c5511cfdfc7fd7632
Stuart Olivera <stuart@stuartolivera.com>
2017-12-29 14:37:42 -0500

    Test api validation failure

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -16,11 +16,22 @@ class ApiControllerTest < ActionDispatch::IntegrationTest
     params[:capacity_data] = { foo: 'bar' }
     post api_import_url, params: params
     assert_response :success
-    body = JSON.parse(@response.body)
+    body = parse_json_body
     assert_equal "bar", body['capacity_data']['foo']
   end
 
+  test "should return error on failed validation" do
+    post api_import_url, params: {}
+    assert_response :unprocessable_entity
+    body = parse_json_body
+    assert_not_empty body['message']
+  end
+
   private
+
+  def parse_json_body
+    JSON.parse(@response.body)
+  end
 
   def valid_params
     model = build(:course_real_time)
