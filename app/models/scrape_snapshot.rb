420165c2dfff9ee776cc40a50bf566872dc29219
Stuart Olivera <stuart@stuartolivera.com>
2018-03-22 01:42:03 -0400

    Write consume_started?, consume_finished? helpers

--- app/models/scrape_snapshot.rb
+++ app/models/scrape_snapshot.rb
@@ -5,4 +5,12 @@ class ScrapeSnapshot < ApplicationRecord
   validates_presence_of :snapshot_at
   validates_presence_of :courses
   # validates_inclusion_of :college, in: %w( ... )
+
+  def consume_started?
+    consume_started_at?
+  end
+
+  def consume_finished?
+    consume_finished_at?
+  end
 end
--- test/models/scrape_snapshot_test.rb
+++ test/models/scrape_snapshot_test.rb
@@ -6,4 +6,16 @@ class ScrapeSnapshotTest < ActiveSupport::TestCase
   should validate_presence_of :college
   should validate_presence_of :snapshot_at
   should validate_presence_of :courses
+
+  test '#consume_started?, #consume_finished?' do
+    snapshot = build(:scrape_snapshot, consume_started_at: nil, consume_finished_at: nil)
+    assert_equal false, snapshot.consume_started?
+    assert_equal false, snapshot.consume_finished?
+    snapshot = build(:scrape_snapshot, consume_started_at: 2.hours.ago, consume_finished_at: nil)
+    assert_equal true, snapshot.consume_started?
+    assert_equal false, snapshot.consume_finished?
+    snapshot = build(:scrape_snapshot, consume_started_at: 2.hours.ago, consume_finished_at: 1.hour.ago)
+    assert_equal true, snapshot.consume_started?
+    assert_equal true, snapshot.consume_finished?
+  end
 end
