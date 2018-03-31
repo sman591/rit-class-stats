421b6cd1cdb200b3dbba8e7fcb7bc6fa40069b87
Stuart Olivera <stuart@stuartolivera.com>
2018-03-31 19:34:41 -0400

    Fix redis connection for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.config.active_job.queue_adapter = :sidekiq
+
+if Rails.env.production?
+  pass = ENV['REDIS_PASSWORD']
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://:#{pass}@redis:6379/15" }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://:#{pass}@redis:6379/15" }
+  end
+end
