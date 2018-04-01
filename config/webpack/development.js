fb7d7c7a510d7f28a40d4927faf75e579e367b96
Stuart Olivera <stuart@stuartolivera.com>
2018-04-01 04:09:44 -0400

    Fix production webpack config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,14 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-environment.loaders.append('eslint', {
-  enforce: 'pre',
-  test: /\.(js|jsx)$/i,
-  exclude: /node_modules/,
-  loader: 'eslint-loader',
-  options: {
-    failOnError: process.env.NODE_ENV !== 'production'
-  }
-})
-
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.append('eslint', {
+  enforce: 'pre',
+  test: /\.(js|jsx)$/i,
+  exclude: /node_modules/,
+  loader: 'eslint-loader',
+  options: {
+    failOnError: process.env.NODE_ENV !== 'production'
+  }
+})
+
 module.exports = environment
