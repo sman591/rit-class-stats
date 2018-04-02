e7922c6d90fd51f8a134333715f89dd5a1e4b363
Stuart Olivera <stuart@stuartolivera.com>
2018-04-02 04:17:59 -0400

    Fix real time relay job test

--- test/jobs/course_real_time_relay_job_test.rb
+++ test/jobs/course_real_time_relay_job_test.rb
@@ -5,16 +5,16 @@ class CourseRealTimeRelayJobTest < ActiveJob::TestCase
 
   test 'job is executed' do
     course_real_time = create(:course_real_time)
-    assert_broadcasts 'course_real_times', 0
+    assert_broadcasts 'real_time:course_real_times', 0
     CourseRealTimeRelayJob.perform_now(course_real_time_id: course_real_time.id)
-    assert_broadcasts 'course_real_times', 1
+    assert_broadcasts 'real_time:course_real_times', 1
   end
 
   test 'job is not executed when a newer object exists' do
     course_real_time = create(:course_real_time, snapshot_at: 2.days.ago, college: 'foo')
     create(:course_real_time, snapshot_at: 1.hour.ago, college: 'foo')
-    assert_broadcasts 'course_real_times', 0
+    assert_broadcasts 'real_time:course_real_times', 0
     CourseRealTimeRelayJob.perform_now(course_real_time_id: course_real_time.id)
-    assert_broadcasts 'course_real_times', 0
+    assert_broadcasts 'real_time:course_real_times', 0
   end
 end
